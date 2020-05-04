f1f28b95dfa06fb7569fcc1a463ccd8c4875001b
SaidShahAhmadi <saidshahahmadi2016@gmail.com>
2020-05-04 08:17:04 -0700

    ok

---
+++ src/componets/movies.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import Like from "../common/like";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
+  handleLike = (movie) => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
+  };
+
+  render() {
+    const { length: count } = this.state.movies;
+
+    if (count === 0) return <p id="red">There are no movies in database</p>;
+    return (
+      <React.Fragment>
+        <p>Shoing {count} movies in the database</p>
+        <table className="table ">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Role</th>
+              <th></th>
+              <th>Action</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />
+                </td>
+
+                <td
+                  onClick={() => this.handleDelete(movie)}
+                  className="btn btn-danger btn-sm"
+                >
+                  Delete
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Movies;

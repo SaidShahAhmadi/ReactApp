5fe461ed696cad58363d90bad0e15c805d50a833
SaidShahAhmadi <saidshahahmadi2016@gmail.com>
2020-06-12 20:37:02 -0700

    searching

--- src/componets/movies.jsx
+++ src/componets/movies.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
-import MoviesTable from "../componets/moviesTable";
+import { Link } from "react-router-dom";
+import MoviesTable from "./moviesTable";
 import ListGroup from "../common/listGroup";
 import Pagination from "../common/pagination";
-import { paginate } from "../utils/paginate";
-import { getMovies } from "../services/fakeMovieService";
+import { getMovies, deleteMovie } from "../services/fakeMovieService";
 import { getGenres } from "../services/fakeGenreService";
+import { paginate } from "../utils/paginate";
 import _ from "lodash";
+import SearchBox from "./searchBox";
 
 class Movies extends Component {
   state = {
@@ -13,6 +15,8 @@ class Movies extends Component {
     genres: [],
     currentPage: 1,
     pageSize: 4,
+    searchQuery: "",
+    selectedGenre: null,
     sortColumn: { path: "title", order: "asc" },
   };
 
@@ -28,7 +32,7 @@ class Movies extends Component {
   };
   //to handle Select list
   handleGenreSelect = (genre) => {
-    this.setState({ selectdGenre: genre, currentPage: 1 });
+    this.setState({ selectedGenre: genre, currentPage: 1 });
   };
 
   //to handle like movies
@@ -45,6 +49,15 @@ class Movies extends Component {
     this.setState({ currentPage: page });
   };
 
+  // to handle searching
+  handleGenreSelect = (genre) => {
+    this.setState({ selectedGenre: genre, searchQuery: "", currentPage: 1 });
+  };
+  //
+  handleSearch = (query) => {
+    this.setState({ searchQuery: query, selectedGenre: null, currentPage: 1 });
+  };
+
   //tho handle sorting by table heading
   handleSort = (sortColumn) => {
     this.setState({ sortColumn });
@@ -54,16 +67,25 @@ class Movies extends Component {
     const {
       pageSize,
       currentPage,
-      selectdGenre,
       sortColumn,
+      selectedGenre,
+      searchQuery,
       movies: allMovies,
     } = this.state;
 
     // to filtered the movies, allMovies section in list
-    const filtered =
-      selectdGenre && selectdGenre._id
-        ? allMovies.filter((m) => m.genre._id === selectdGenre._id)
-        : allMovies;
+    // const filtered =
+    //   selectedGenre && selectedGenre._id
+    //     ? allMovies.filter((m) => m.genre._id === selectedGenre._id)
+    //     : allMovies;
+
+    let filtered = allMovies;
+    if (searchQuery)
+      filtered = allMovies.filter((m) =>
+        m.title.toLowerCase().startsWith(searchQuery.toLowerCase())
+      );
+    else if (selectedGenre && selectedGenre._id)
+      filtered = allMovies.filter((m) => m.genre._id === selectedGenre._id);
 
     //sorting and lodash
     const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
@@ -77,7 +99,7 @@ class Movies extends Component {
   render() {
     //object Destracturing
     const { length: count } = this.state.movies;
-    const { pageSize, currentPage, sortColumn } = this.state;
+    const { pageSize, currentPage, sortColumn, searchQuery } = this.state;
 
     if (count === 0) return <p id="red">There are no movies in database.</p>;
 
@@ -88,13 +110,23 @@ class Movies extends Component {
         <div className="col-2">
           <ListGroup
             items={this.state.genres}
-            selectedItem={this.state.selectdGenre}
+            selectedItem={this.state.selectedGenre}
             onItemSelect={this.handleGenreSelect}
           />
         </div>
 
         <div className="col">
+          <Link
+            to="/movies/new"
+            className="btn btn-primary"
+            style={{ marginBottom: 20 }}
+          >
+            New Movie
+          </Link>
           <p>Shoing {totalCount} movies in the database</p>
+
+          {/* searchbox Component */}
+          <SearchBox value={searchQuery} onChange={this.handleSearch} />
 
           <MoviesTable
             movies={movies}
